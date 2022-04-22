3ceb76b1d1997a81b226e1ee25e03a0df409c8cf
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2022-04-22 21:49:01 +0500

    adds Admin entity

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import { Meal } from "./entries/Meal";
 import { Day } from "./entries/Day";
 import { Category } from "./entries/Category";
+import { Admin } from "./entries/Admin";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -9,7 +10,7 @@ export const AppDataSource = new DataSource({
     database: process.env.DATABASE_NAME!,
     username: process.env.DATABASE_USER!,
     password: process.env.DATABASE_PASS!,
-    entities: [Meal, Day, Category],
+    entities: [Meal, Day, Category, Admin],
     synchronize: true,
     logging: false,
 })
@@ -17,3 +18,4 @@ export const AppDataSource = new DataSource({
 export const MealRepository = AppDataSource.getRepository(Meal);
 export const DayRepository = AppDataSource.getRepository(Day);
 export const CategoryRepository = AppDataSource.getRepository(Category);
+export const AdminRepository = AppDataSource.getRepository(Admin);
