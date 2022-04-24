97d2441f51735637eefe6193b4af00c509918f24
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2022-04-24 18:38:21 +0500

    adds Order entity

--- backend/src/database/entries/Order.ts
+++ backend/src/database/entries/Order.ts
@@ -1,13 +1,21 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm"
-
 @Entity("orders")
 export class Order {
     @PrimaryGeneratedColumn()
-    order_id!: number
+    order_id!: number;
 
     @Column()
-    order_token!: string
+    order_token!: string;
+
+    @Column()
+    order_email!: string;
+
+    @Column()
+    order_phone!: string;
+
+    @Column()
+    order_verified!: boolean;
 
     @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
-    order_createdAt!: Date
+    order_createdAt!: Date;
 }
--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -4,6 +4,7 @@ import { Day } from "./entries/Day";
 import { Category } from "./entries/Category";
 import { Admin } from "./entries/Admin";
 import { Order } from "./entries/Order";
+import { OrderMeal } from "./entries/OrderMeal";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     database: process.env.DATABASE_NAME!,
     username: process.env.DATABASE_USER!,
     password: process.env.DATABASE_PASS!,
-    entities: [Meal, Day, Category, Admin, Order],
+    entities: [Meal, Day, Category, Admin, Order, OrderMeal],
     synchronize: true,
     logging: false,
 })
@@ -20,3 +21,5 @@ export const MealRepository = AppDataSource.getRepository(Meal);
 export const DayRepository = AppDataSource.getRepository(Day);
 export const CategoryRepository = AppDataSource.getRepository(Category);
 export const AdminRepository = AppDataSource.getRepository(Admin);
+export const OrderRepository = AppDataSource.getRepository(Order);
+export const OrderMealRepository = AppDataSource.getRepository(OrderMeal);
